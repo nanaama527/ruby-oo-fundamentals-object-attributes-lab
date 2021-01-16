c21a1fcd27176b669b323ab687fec921fa91f9e5
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-15 19:06:51 -0500

    done

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+    attr_accessor :name,:job
+    
+    def name=(name)
+        @name = name
+    end
+
+    def name 
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
+
+
+end    
